82859f39afb7c7479ac9b93522094afa8cef3de4
Unpunished <idontwanttodie2@gmail.com>
2019-12-04 03:17:20 +0400

    added missing files

---
+++ src/main/java/ru/kpfu/icmit/server4/util/MyDateFormat.java
@@ -0,0 +1,7 @@
+package ru.kpfu.icmit.server4.util;
+
+import java.text.SimpleDateFormat;
+
+public class MyDateFormat {
+    public static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SXXX");
+}
